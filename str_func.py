63bbfa0321f36ad2bb25713fd1b775587f62aaef
DeafProger <usvval@yandex.ru>
2024-04-02 01:09:36 +0700

    conflict resolved

--- str_func.py
+++ str_func.py
@@ -1,4 +1,13 @@
 def upstring(source):
+    """
+        Return source string in upper case
+    """
     return source.upper()
 
+
+def titlestr(source):
+    """
+        Return source string in title case
+    """
+    return source.title()
 
