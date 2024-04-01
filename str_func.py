1d4c271cd075e7a51d2fc197a07ec762bbcffd4b
DeafProger <usvval@yandex.ru>
2024-04-02 00:16:33 +0700

    add docstrings into str_func.py

--- str_func.py
+++ str_func.py
@@ -1,4 +1,12 @@
 def upstring(source):
+    """
+        sdcfwdReturn source string in upper case
+    """
     return source.upper()
 
 
+def titlestr(source):
+    """
+        Return source string in title case
+    """
+    return source.title()
